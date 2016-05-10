9379e35c17305b359da56d3b86f36aa6dda54298
Tulio Faria <tuliofaria@gmail.com>
2016-05-10 01:00:05 -0300

    Fix.

--- public/js/agency.js
+++ public/js/agency.js
@@ -26,18 +26,20 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 
-$(function(){
+var app = angular.module('app', []);
+app.controller('ResultsCtrl', ['$scope', function($scope){
+	$scope.results = [{"name":"haiyk","data":[{"_id":{"id":"4030160","moment":"madrugada","league":"PLATINUM","division":"V"},"moment":42,"summoners":[{"id":"1071567","moment":"madrugada","league":"PLATINUM","division":"V"}]},{"_id":{"id":"4030160","moment":"noite","league":"PLATINUM","division":"V"},"moment":30,"summoners":[{"id":"1711331","moment":"noite","league":"PLATINUM","division":"III"},{"id":"632567","moment":"noite","league":"PLATINUM","division":"V"},{"id":"3997246","moment":"noite","league":"PLATINUM","division":"IV"},{"id":"402122","moment":"noite","league":"PLATINUM","division":"V"},{"id":"1786628","moment":"noite","league":"PLATINUM","division":"V"},{"id":"1169331","moment":"noite","league":"PLATINUM","division":"V"},{"id":"3511509","moment":"noite","league":"PLATINUM","division":"IV"}]},{"_id":{"id":"4030160","moment":"tarde","league":"PLATINUM","division":"V"},"moment":9,"summoners":[]}]}];
+
 	var socket = io();
-
-	socket.on('connect', function(s){
-
-	});
 	socket.on('searchReady', function(s){
 		$.get('/users/getFriends', s, function(data){
-			console.log(data);
-			$("#summomers").append(data);
+			$scope.$apply(function () {
+				$scope.results.push({
+					name: s.name,
+					data: data
+				});
+			});
 		});
-		console.log("serachReady", s);
 	});
 
 	$("#inProgress").hide();
@@ -53,9 +55,8 @@ $(function(){
 
 
 			$.get('/users/find', { processId: processId, name: name, region: region}, function(ret){
-				//$("#summomers").append("<div id='proc"+processId+">'<h4>"+name+"</h4><p>In progress...</p></div>");
 			});
 
 		}
 	});
-});
+}]);
